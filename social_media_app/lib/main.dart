cb256f28e0c0f8ebd855dd0c78ab6f2d98b6e848
PyiTheimKyaw <92578990+PyiTheimKyaw@users.noreply.github.com>
2022-06-03 22:20:45 +0630

    implemented in app message

--- social_media_app/lib/main.dart
+++ social_media_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_installations/firebase_installations.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:social_media_app/data/model/authentication_model.dart';
@@ -13,6 +14,8 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   FCMService().listenForMessages();
+  var firebaseInstallationId=await FirebaseInstallations.id ?? "Unknown installation id";
+  debugPrint("Firebase installation id => $firebaseInstallationId");
   runApp(const MyApp());
 }
 
